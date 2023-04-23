4e59794e31bbac9048154ac9c56536790ff214d8
GopalSaraf <sarafgopal37@gmail.com>
2023-04-23 16:20:49 +0530

    Final Upload - 1

--- app.py
+++ app.py
@@ -8,42 +8,45 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 THRESHOLD = 0.05
-MODEL_FILEPATH = os.path.join(os.path.dirname(__file__), 'model.pkl')
+MODEL_FILEPATH = os.path.join(os.path.dirname(__file__), "model.pkl")
 
-with open(MODEL_FILEPATH, 'rb') as f:
+with open(MODEL_FILEPATH, "rb") as f:
     gbm = pickle.load(f)
     state_dict = gbm.__getstate__()
-    classes_array = state_dict['classes_']
-    features = state_dict['feature_names_in_']
+    classes_array = state_dict["classes_"]
+    features = state_dict["feature_names_in_"]
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def default():
-    return jsonify({
-        'message': 'Welcome to the MediAI API!'
-    })
+    return jsonify({"message": "Welcome to the MediAI API!"})
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    if not request.json or 'symptoms' not in request.json:
-        return jsonify({
-            'message': 'Invalid request, symptoms field is required'
-        })
+    if not request.json or "symptoms" not in request.json:
+        return jsonify({"message": "Invalid request, symptoms field is required"})
 
-    symptoms = request.json['symptoms']
-    threshold = float(request.json.get('threshold', THRESHOLD))
+    symptoms = request.json["symptoms"]
+    threshold = float(request.json.get("threshold", THRESHOLD))
 
     if not symptoms:
-        return jsonify({
-            'output': [],
-            'filtered_output': [],
-            'message': 'No matches found'
-        })
-    
-    coded_features = [np.where(features == keyword)[0][0] for keyword in symptoms if keyword in features]
-    sample_x = np.array([i / coded_features[coded_features.index(i)] 
-                         if i in coded_features else 0 
-                         for i in range(len(features))]).reshape(1, -1)
-    
+        return jsonify(
+            {"output": [], "filtered_output": [], "message": "No matches found"}
+        )
+
+    coded_features = [
+        np.where(features == keyword)[0][0]
+        for keyword in symptoms
+        if keyword in features
+    ]
+    sample_x = np.array(
+        [
+            i / coded_features[coded_features.index(i)] if i in coded_features else 0
+            for i in range(len(features))
+        ]
+    ).reshape(1, -1)
+
     probs = gbm.predict_proba(sample_x.reshape(1, -1))[0]
     output = np.column_stack((classes_array, probs.astype(float)))
 
@@ -55,19 +58,20 @@ def predict():
 
     if len(filtered_output) == 0:
         filtered_output = output[:1]
-    
-    return jsonify({
-        'output': output.tolist(),
-        'filtered_output': filtered_output.tolist(),
-        'message': f'{len(filtered_output)} matches found'
-    })
 
-@app.route('/metadata', methods=['GET'])
+    return jsonify(
+        {
+            "output": output.tolist(),
+            "filtered_output": filtered_output.tolist(),
+            "message": f"{len(filtered_output)} matches found",
+        }
+    )
+
+
+@app.route("/metadata", methods=["GET"])
 def metadata():
-    return jsonify({
-        'classes': classes_array.tolist(),
-        'features': features.tolist()
-    })
+    return jsonify({"classes": classes_array.tolist(), "features": features.tolist()})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
